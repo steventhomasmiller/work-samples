47dbeb20ad151b5a2fa7c076a1e05b9d8eb23cdf
steventhomasmiller <steventhomasmiller@gmail.com>
2014-11-16 13:46:06 -0800

    Create television.php

---
+++ television.php
@@ -0,0 +1,67 @@
+<div id="main" class="sidebar-main">
+	<div class="width-container sidebar-background">
+		<div id="additional-background">
+<div id="content-container">
+			<p>Below are excerpts of my work in television programming, sorted by the channel on which they appeared.</p>
+<p>Click on the program title to watch.</p>
+<div class="section" style="margin-bottom: 100px;">
+<div class="channel" style="margin-bottom: 30px;"><img alt="key_art_biography" src="http://henrynevisonproductions.com/wp-content/uploads/2013/09/key_art_biography-e1379523442593-300x103.jpg" width="210" height="72" /></p>
+</div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>Elizabeth Hurley</h3>
+<p><iframe src="//player.vimeo.com/video/75397244" width="640" height="360" frameborder="0" title="&quot;Elizabeth Hurley: Famous for Being Famous&quot; (3 minute excerpt)" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+</div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>Eric Clapton</h3>
+<p><iframe src="//player.vimeo.com/video/75395364" width="640" height="360" frameborder="0" title="Eric Clapton: &quot;Only the Messenger&quot;  (3 minute excerpt)" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+</div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>Pete Townsend</h3>
+<p><iframe src="//player.vimeo.com/video/75396226" width="640" height="360" frameborder="0" title="Pete Townsend: &quot;Can&#039;t Explain&quot; (3 minute excerpt)" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+</div>
+</div>
+<div class="section" style="margin-bottom: 100px;">
+<div class="channel" style="margin-bottom: 30px;"><img alt="Military History Channel" src="http://henrynevisonproductions.com/wp-content/uploads/2013/09/Military_History_Channel.png" width="162" height="108" /> <img class="alignnone size-full wp-image-43" alt="History International" src="http://henrynevisonproductions.com/wp-content/uploads/2013/09/logo_historyint.jpg" width="98" height="78" /></p>
+</div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>Hero Ships: The LST</h3>
+<p><iframe src="//player.vimeo.com/video/75417336" width="640" height="360" frameborder="0" title="Hero Ships: &quot;The LST&quot;" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+</div>
+</div>
+<div class="section" style="margin-bottom: 100px;">
+<div class="channel" style="margin-bottom: 30px;"><img class="alignnone size-full wp-image-45" alt="History Channel Spanish" src="http://henrynevisonproductions.com/wp-content/uploads/2013/09/imgres.jpeg" width="158" height="115" /></div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>The Mexican Revolution</h3>
+<p><iframe src="//player.vimeo.com/video/75706078" width="640" height="360" frameborder="0" title="&quot;The Mexican Revolution&quot;, 5 minute excerpt" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+</div>
+</div>
+<div class="section" style="margin-bottom: 100px;">
+<div class="channel" style="margin-bottom: 30px;"><img class="alignnone size-full wp-image-46" alt="images" src="http://henrynevisonproductions.com/wp-content/uploads/2013/09/images.jpeg" width="200" height="120" /></a></div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>World War II in HD</h3>
+<p><iframe src="//player.vimeo.com/video/75419294" width="640" height="360" frameborder="0" title="WWII in HD: Episode 6 &quot;The Air War&quot; (3 minute excerpt)" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+</div>
+</div>
+<div class="section" style="margin-bottom: 100px;">
+<div class="channel" style="margin-bottom: 30px;"><img class="alignnone  wp-image-47" alt="Military Discovery Channel" src="http://henrynevisonproductions.com/wp-content/uploads/2013/09/military_channel_discovery.png.300x300_q85.png" width="210" height="105" /></div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>Target America</h3>
+<p><iframe src="//player.vimeo.com/video/75399069" width="640" height="360" frameborder="0" title="&quot;Target America&quot; (3 minute excerpt)" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+</div>
+</div>
+<div class="section" style="margin-bottom: 100px;">
+<div class="channel" style="margin-bottom: 30px;"><img src="http://henrynevisonproductions.com/wp-content/uploads/2013/09/NBCUT_Logo_nbc10philadelphia.png" alt="NBC Logo" width="221" height="124" class="alignnone size-full wp-image-48" /></div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>Rewind in Time (Emmy nominated)</h3>
+<p><iframe src="//player.vimeo.com/video/75412179" width="640" height="360" frameborder="0" title="&quot;Rewind in Time&quot; (3 minute excerpt)" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></p>
+</div>
+</div>
+<div class="section" style="margin-bottom: 100px;">
+<div class="channel" style="margin-bottom: 30px;"><img src="http://henrynevisonproductions.com/wp-content/uploads/2013/09/mind-logo1.jpg" alt="mind-logo" width="150" height="62" class="alignnone size-full wp-image-64" /></a></div>
+<div id="video" style="margin-bottom: 50px;">
+<h3>&#8220;World of Dance in Philadelphia” – multi-part series  (Emmy nominated).</h3>
+<p><iframe width="560" height="315" src="//www.youtube.com/embed/videoseries?list=PL1BADA83E06D052FC" frameborder="0" allowfullscreen></iframe></p>
+</div>
+</div>
+	
+				<div class="clearfix"></div>
